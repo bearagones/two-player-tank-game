aa16873eb25e2207a42f49b00606ebc5ac617e04
bearagones <baragones@mail.sfsu.edu>
2022-04-15 01:21:49 -0700

    Updated Launcher.java

--- src/main/java/tankgame/Launcher.java
+++ src/main/java/tankgame/Launcher.java
@@ -1,7 +1,6 @@
 package tankgame;
 
 
-import tankgame.game.TRE;
 import tankgame.gamescreens.EndGamePanel;
 import tankgame.gamescreens.StartMenuPanel;
 
@@ -49,7 +48,7 @@ public class Launcher {
 
     public Launcher(){
         this.jf = new JFrame();             // creating a new JFrame object
-        this.jf.setTitle("Tank Wars Game"); // setting the title of the JFrame window.
+        this.jf.setTitle("tankgame.gameobjects.Tank Wars Game"); // setting the title of the JFrame window.
         this.jf.setLocationRelativeTo(null); // this will open the window in the center of the screen.
         this.jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // when the GUI is closed, this will also shutdown the VM
     }
