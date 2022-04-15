8c3160a29fd8b1c7570bc181bed86ddf7410e749
bearagones <baragones@mail.sfsu.edu>
2022-04-15 01:20:38 -0700

    Updated GameObject.java

--- src/main/java/tankgame/gameobjects/GameObject.java
+++ src/main/java/tankgame/gameobjects/GameObject.java
@@ -1,8 +1,23 @@
 package tankgame.gameobjects;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
-public abstract class GameObject {
+public abstract class GameObject implements Collidable {
+    public int x;
+    public int y;
+    public BufferedImage image;
+    public Rectangle hitBox;
+
+    public GameObject(int x, int y, BufferedImage image) {
+        this.x = x;
+        this.y = y;
+        this.image = image;
+        this.hitBox = new Rectangle(x, y, this.image.getWidth(), this.image.getHeight());
+    }
 
     public abstract void drawImage(Graphics g);
+
+    public abstract void detectCollision(Collidable object);
+
 }
