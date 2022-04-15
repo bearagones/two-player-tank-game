4b76e33b642cddd813b13525ff5c13449d828e9d
bearagones <baragones@mail.sfsu.edu>
2022-04-15 01:20:18 -0700

    Created Collidable.java

---
+++ src/main/java/tankgame/gameobjects/Collidable.java
@@ -0,0 +1,10 @@
+package tankgame.gameobjects;
+
+import java.awt.*;
+
+public interface Collidable {
+
+    //public void detectCollision(Collidable object);
+
+    public Rectangle getHitBox();
+}
