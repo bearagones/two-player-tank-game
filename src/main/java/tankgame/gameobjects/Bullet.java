34c3a6d6dee45c19413c4198d4a1b7c4da53f23b
bearagones <baragones@mail.sfsu.edu>
2022-04-15 01:20:27 -0700

    Updated Bullet.java

--- src/main/java/tankgame/gameobjects/Bullet.java
+++ src/main/java/tankgame/gameobjects/Bullet.java
@@ -1,4 +1,36 @@
 package tankgame.gameobjects;
 
-public class Bullet {
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class Bullet extends GameObject {
+    private int x;
+    private int y;
+    private BufferedImage image;
+    private Rectangle hitBox;
+    private boolean collided;
+
+    public Bullet(int x, int y, BufferedImage image) {
+        super(x, y, image);
+    }
+
+    @Override
+    public void drawImage(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+        g2.drawImage(this.image, x, y, null);
+    }
+
+    @Override
+    public void detectCollision(Collidable object) {
+        if (object instanceof Bullet) {
+            if (this.getHitBox().intersects(object.getHitBox())) {
+                collided = true;
+            }
+        }
+    }
+
+    @Override
+    public Rectangle getHitBox() {
+        return hitBox;
+    }
 }
