7a33e85e84fb43a646faa5d446a34001316c372c
bearagones <baragones@mail.sfsu.edu>
2022-04-19 13:32:48 -0700

    Updated PowerUp.java

--- src/main/java/tankgame/gameobjects/powerup/PowerUp.java
+++ src/main/java/tankgame/gameobjects/powerup/PowerUp.java
@@ -1,15 +1,11 @@
 package tankgame.gameobjects.powerup;
 
-import tankgame.gameobjects.Collidable;
 import tankgame.gameobjects.GameObject;
 import tankgame.gameobjects.Tank;
-import tankgame.gameobjects.wall.Wall;
 
 import java.awt.image.BufferedImage;
 
 public abstract class PowerUp extends GameObject {
-
-    private Tank tank;
 
     public PowerUp(int x, int y, BufferedImage image) {
         super(x, y, image);
