15d870fab514790ffcc70bad0568c3e5ac289474
bearagones <baragones@mail.sfsu.edu>
2022-04-19 12:47:27 -0700

    Updated Bullet.java
    - Removed visual hitbox

--- src/main/java/tankgame/gameobjects/Bullet.java
+++ src/main/java/tankgame/gameobjects/Bullet.java
@@ -34,8 +34,6 @@ public class Bullet extends GameObject implements Moveable {
         rotation.rotate(Math.toRadians(angle), this.image.getWidth() / 2.0, this.image.getHeight() / 2.0);
         Graphics2D g2d = (Graphics2D) g;
         g2d.drawImage(this.image, rotation, null);
-        g2d.setColor(Color.BLUE);
-        g2d.drawRect(x, y, this.image.getWidth(), this.image.getHeight());
     }
 
     @Override
