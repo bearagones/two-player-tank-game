c91cf6151a4dafb575803bfc294645f801ae925a
bearagones <baragones@mail.sfsu.edu>
2022-04-19 18:06:02 -0700

    Updated Wall.java

--- src/main/java/tankgame/gameobjects/wall/Wall.java
+++ src/main/java/tankgame/gameobjects/wall/Wall.java
@@ -1,6 +1,9 @@
 package tankgame.gameobjects.wall;
 
+import tankgame.gameobjects.Bullet;
+import tankgame.gameobjects.Collidable;
 import tankgame.gameobjects.GameObject;
+import tankgame.gameobjects.Tank;
 
 import java.awt.image.BufferedImage;
 
@@ -8,6 +11,17 @@ public abstract class Wall extends GameObject {
 
     public Wall(int x, int y, BufferedImage image) {
         super(x, y, image);
+    }
+
+    @Override
+    public void detectCollision(Collidable object) {
+        if (this.getHitBox().intersects(object.getHitBox())) {
+            if (object instanceof Tank) {
+                collided = false;
+            } else if (object instanceof Bullet) {
+                collided = true;
+            }
+        }
     }
 
     public abstract boolean isBreakable();
